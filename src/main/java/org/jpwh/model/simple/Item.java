360662a47673c88d50af6de4a532ee7338357a09
thaiNguyen004 <nguyenntph33935@fpt.edu.vn>
2023-09-08 18:39:39 +0700

    up

--- src/main/java/org/jpwh/model/simple/Item.java
+++ src/main/java/org/jpwh/model/simple/Item.java
@@ -1,30 +1,19 @@
 package org.jpwh.model.simple;
 
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-/*
-Đây là lớp thực thể cơ bản nhất, được đánh dấu là "có khả năng bền vững" bằng chú thích @Entity, và có ánh xạ @Id
-cho thuộc tính định danh của cơ sở dữ liệu. Lớp này mặc định ánh xạ vào một bảng có tên là ITEM trong mô hình cơ sở dữ liệu.
-*/
-
 @Entity
-@Table(name = "ITEM")
+/*
+    Using vendor extension
+    Sử dụng annotation riêng của Hibernate không nằm trong API Java Persistence
+*/
+@org.hibernate.annotations.Cache(
+        usage = CacheConcurrencyStrategy.READ_WRITE
+)
 public class Item {
-    @Id
-    @GeneratedValue (generator = "ID_GENERATOR")
-    protected Long id;
-    protected String name;
 
-    // Không bắt buộc nhưng hữu ích
-    // Optional but useful
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/org/jpwh/model/simple/package-info.java
+++ src/main/java/org/jpwh/model/simple/package-info.java
@@ -1,19 +1,15 @@
 package org.jpwh.model.simple;
 
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
-
-@GenericGenerator(
-        name = "ID_GENERATOR",
-        strategy = "enhanced-sequence",
-        parameters = {
-                @Parameter(
-                        name = "sequence_name",
-                        value = "JPWH_SEQUENCE"
-                ),
-                @Parameter(
-                        name = "initial_value",
-                        value = "1000"
-                )
-        }
-)
+@org.hibernate.annotations.NamedQueries({
+        @org.hibernate.annotations.NamedQuery(
+                name = "findItemsOrderByName",
+                query = "select i from Item i order by i.name asc"
+        )
+        ,
+        @org.hibernate.annotations.NamedQuery(
+                name = "findItemBuyNowPriceGreaterThan",
+                query = "select i from Item i where i.buyNowPrice > :price",
+                timeout = 60,
+                comment = "Custom SQL commment"
+        )
+})
